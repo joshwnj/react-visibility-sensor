c147378717341d711472181c53902476b95588dc
Stefan B <Stefan.bie@live.de>
2015-03-30 12:18:29 +0200

    - rename jsx to js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var jsx = require('gulp-jsx');
+var rename = require("gulp-rename")
 
 gulp.task('build', function() {
   return gulp.src('visibility-sensor.jsx')
     .pipe(jsx())
+    .pipe( rename(function (path) {
+      path.extname = ".js"
+    }))
     .pipe(gulp.dest('dist'));
 });
