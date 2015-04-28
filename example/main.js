417c0d0b25af180bf26f087c9e21eabc763842d3
Josh Johnston <josh@x-team.com>
2015-04-28 16:06:52 +1000

    updated example to use containment prop

--- example/main.js
+++ example/main.js
@@ -1,5 +1,4 @@
 /** @jsx React.DOM */
-
 'use strict';
 
 var React = require('react');
@@ -22,12 +21,20 @@ var Example = React.createClass({
     return (
       <div>
         <p className='msg'>{this.state.msg}</p>
+        <div className='before'></div>
         <div className='sensor'>
-            <VisibilitySensor onChange={onChange} />
+          <VisibilitySensor containment={this.props.containment} onChange={onChange} />
         </div>
+        <div className='after'></div>
       </div>
     );
   }
 });
 
 React.render(React.createElement(Example), document.getElementById('example'));
+
+var container = document.getElementById('example-container');
+var elem = container.querySelector('.inner');
+container.scrollTop = 320;
+container.scrollLeft = 320;
+React.render(React.createElement(Example, { containment: container }), elem);
