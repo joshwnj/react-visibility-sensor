d0ced05b8735e870010580d420fc16d50b5d27e7
Andy Edwards <jedwards@fastmail.com>
2016-07-19 13:54:32 -0500

    use simpler logic for partialVisible
    The existing logic was overkill

--- visibility-sensor.js
+++ visibility-sensor.js
@@ -116,16 +116,9 @@ module.exports = React.createClass({
 
     // check for partial visibility
     if (this.props.partialVisibility) {
-      var partialVertical =
-          (rect.top >= containmentRect.top && rect.top <= containmentRect.bottom)
-          || (rect.bottom >= containmentRect.top && rect.bottom <= containmentRect.bottom)
-          || (rect.top <= containmentRect.top && rect.bottom >= containmentRect.bottom);
-
-      var partialHorizontal =
-          (rect.left >= containmentRect.left && rect.left <= containmentRect.right)
-          || (rect.right >= containmentRect.left && rect.right <= containmentRect.right);
-
-      var partialVisible = partialVertical && partialHorizontal;
+      var partialVisible =
+          rect.top <= containmentRect.bottom && rect.bottom >= containmentRect.top &&
+          rect.left <= containmentRect.right && rect.right >= containmentRect.left;
 
       // if we have minimum top visibility set by props, lets check, if it meets the passed value
       // so if for instance element is at least 200px in viewport, then show it.
